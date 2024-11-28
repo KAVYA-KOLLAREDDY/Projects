cefc75d7a78024034d8e38ed021afb298c7dbdfa
Kavya <113453872+KAVYA-KOLLAREDDY@users.noreply.github.com>
2024-11-28 03:59:33 -0500

    Add files via upload

---
+++ Suresh Sir project/managerEmployees/src/main/java/com/project/managerEmployees/ManagerEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.project.managerEmployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerEmployeesApplication.class, args);
+	}
+
+}
